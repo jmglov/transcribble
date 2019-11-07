27da3028556c78f78a6888821174f255edd2b32f
Josh Glover <joshua.glover@klarna.com>
2019-11-07 11:50:41 +0100

    Insert timestamps for single-speaker podcasts

--- src/transcribble/core.clj
+++ src/transcribble/core.clj
@@ -29,31 +29,63 @@
 (defn finalise-part [part]
   (update part :words #(string/join " " %)))
 
-(defn speaker-splitter [speaker-at]
-  (fn [[parts current-part] {:keys [type alternatives start_time]}]
-    (let [word (-> alternatives first :content)
-          speaker (speaker-at start_time)
-          current-part (if (or (:start-time current-part) (nil? start_time))
-                         current-part
-                         (assoc current-part :start-time start_time))]
-      (cond
-        (= "punctuation" type)
-        [parts (update current-part :words append-punctuation word)]
+(defn split-parts [{:keys [split-duration-secs speakers] :as config}
+                   speaker-at pronunciations]
+  (println "Number of speakers:" (count speakers))
+  (println "Splitting every" split-duration-secs "seconds")
+  (let [->float
+        #(try (Float. %) (catch Exception _ 0.0))
 
-        (or (= (:speaker current-part) speaker) (empty? (:words current-part)))
-        [parts (update current-part :words conj word)]
+        within-split-duration?
+        (fn [{:keys [start-time]} pronunciation]
+          (if (:start_time pronunciation)
+            (< (- (->float (:start_time pronunciation)) start-time)
+               split-duration-secs)
+            true))
 
-        :else
-        [(conj parts (finalise-part current-part))
-         {:speaker speaker, :words [word]}]))))
+        splitter
+        (fn [[parts current-part following-punctuation?]
+             {:keys [type alternatives start_time] :as pronunciation}]
+          (let [word (-> alternatives first :content)
+                current-part (if (or (:start-time current-part) (nil? start_time))
+                               current-part
+                               (do
+                                 (println "Setting start time to" start_time)
+                                 (assoc current-part :start-time (->float start_time))))]
+            (cond
+              (= "punctuation" type)
+              [parts (update current-part :words append-punctuation word) true]
+
+              (empty? (:words current-part))
+              [parts (update current-part :words conj word) false]
+
+              (and (= (count speakers) 1)
+                   (not following-punctuation?))
+              [parts (update current-part :words conj word) false]
+
+              (and (= (count speakers) 1)
+                   (within-split-duration? current-part pronunciation))
+              [parts (update current-part :words conj word) false]
+
+              (and (> (count speakers) 1)
+                   (= (:speaker current-part) (speaker-at (:start_time pronunciation))))
+              [parts (update current-part :words conj word) false]
+
+              :else
+              [(conj parts (finalise-part current-part))
+               {:speaker (speaker-at start_time)
+                :start-time (->float start_time)
+                :words [word]}
+               false])))]
+    (reduce splitter
+            [[] {:words []} false]
+            pronunciations)))
 
 (defn load-transcribe-json [config filename]
   (let [{:keys [results]} (load-json-file filename)
         speaker-at (speakers/build-speaker-at config results)
         pronunciations (:items results)
-        [parts last-part] (->> pronunciations
-                               (reduce (speaker-splitter speaker-at)
-                                       [[] {:words []}]))
+        [parts last-part] (split-parts config speaker-at pronunciations)
         all-parts (conj parts (finalise-part last-part))]
     (if (= 1 (count all-parts))
       [(assoc (first all-parts) :speaker (speakers/->speaker-label 0))]
