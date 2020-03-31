7485d937fa1ea5eda53b68e09648c4ddf95c4a98
Josh Glover <jmglov@hey.com>
2020-03-31 07:27:17 +0200

    Add --fixup-otr option

--- src/transcribble/main.clj
+++ src/transcribble/main.clj
@@ -4,11 +4,15 @@
             [transcribble.speakers :as speakers])
   (:gen-class))
 
-(defn -main [transcribe-filename output-filename media-filename speaker-names
-             & [config-filename]]
-  (let [speakers (speakers/make-speakers-map speaker-names)
-        config (-> (core/load-config config-filename)
-                   (assoc :speakers speakers))
-        parts (core/load-transcribe-json config transcribe-filename)
-        formatted-parts (format/format-parts config media-filename parts)]
-    (spit output-filename formatted-parts)))
+(defn -main [& args]
+  (if (= "--fixup-otr" (first args))
+    (let [[_ filename] args]
+      (format/fixup-otr filename))
+    (let [[transcribe-filename output-filename media-filename speaker-names
+           & [config-filename]] args
+          speakers (speakers/make-speakers-map speaker-names)
+          config (-> (core/load-config config-filename)
+                     (assoc :speakers speakers))
+          parts (core/load-transcribe-json config transcribe-filename)
+          formatted-parts (format/format-parts config media-filename parts)]
+      (spit output-filename formatted-parts))))
